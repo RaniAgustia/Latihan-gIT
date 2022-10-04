6c10bbbe101e5e294148895aa47de1b040b85fda
WP2_RaniAgustia <raniagustia178@gmail.com>
2022-10-04 09:04:03 +0700

    Tugas Pertemuan 3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Rani Agustia
+ Saya lahir di Ketapang
+ hobi saya adalah makan dan jalan-jalan";
+ }
+}
+?>
---
+++ application/views/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+<head>
+<title>Selamat Datang</title>
+</head>
+<body>
+    <h1>Perkenalkan</h1>
+    <p>Nama saya Rani Agustia</p>
+    <p>Saya lahir di Ketapang</p>
+    <p>hobi saya adalah makan dan jalan-jalan</p>
+</body>
+</html>
